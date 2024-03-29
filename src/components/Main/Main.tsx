ff417b16c6c2f6a7cd5eb870ac02671775481129
a.lihtgolt <a.lihtgolt@gmail.com>
2024-03-29 23:56:12 +0200

    Map added

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,11 +1,13 @@
 import Products from "../Products/Products";
 import Delivery from "../Delivery/Delivery";
+import Map from "../Map/Map";
 
 const Main = () => {
     return (
         <main>
             <Products/>
             <Delivery/>
+            <Map/>
         </main>
     );
 };
