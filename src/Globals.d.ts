67a376c42c3c7e09a48072b5008df14e0ffa585d
a.lihtgolt <a.lihtgolt@gmail.com>
2024-04-01 23:04:16 +0300

    db update

--- src/Globals.d.ts
+++ src/Globals.d.ts
@@ -1,5 +1,8 @@
 declare module "*.module.css";
 declare module '*.png';
+declare module '*.jpeg';
+declare module '*.jpg';
+declare module '*.webp';
 declare module "react-transition-group";
 declare module "@fortawesome/react-fontawesome";
 declare module "@fortawesome/free-solid-svg-icons";
