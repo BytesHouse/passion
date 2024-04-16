dd6273c270cc3968a1664d0aaaa0a020b2e4483b
Alexandr Evstiffev <128703137+Faviun@users.noreply.github.com>
2024-04-17 00:20:14 +0300

    newCommit (#2)

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,29 +1,35 @@
-import cls from './Cart.module.css';
-import {useSelector} from "react-redux";
-import {RootState} from "../../store/store";
+import cls from "./Cart.module.css";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
 import CartIcon from "../../assets/icons/CartIcon/CartIcon";
+import rub from "../../assets/images/ruble.png";
 
 interface CartProps {
     show: () => void;
 }
-const Cart = ({show}:CartProps) => {
-    const { cart, total } = useSelector((state: RootState) => state.cart)
-    const formatedPrice = Number(total ?? 0).toFixed(2)
+const Cart = ({ show }: CartProps) => {
+    const { cart, total } = useSelector((state: RootState) => state.cart);
+    const formatedPrice = Number(total ?? 0).toFixed(2);
     const length =
         cart.length <= 3
-            ?
-            {background: '#ef3a65'}
-            :
-            cart.length > 3 && cart.length <= 6
-                ?
-                {background: '#f7d22d', color: 'black', border: '2px solid black'}
-                :
-                {background: 'green'}
+            ? { background: "#ef3a65" }
+            : cart.length > 3 && cart.length <= 6
+            ? {
+                  background: "#f7d22d",
+                  color: "black",
+                  border: "2px solid black",
+              }
+            : { background: "green" };
     return (
         <div onClick={show} className={cls.cart}>
-            <CartIcon/>
-            <p style={length} className={cls.count}>{cart.length ?? 0}</p>
-            <p>{formatedPrice} ₽</p>
+            <CartIcon />
+            <p style={length} className={cls.count}>
+                {cart.length ?? 0}
+            </p>
+            <p className={cls.totalSum}>
+                {formatedPrice}{" "}
+                <img className={cls.rubLogo} src={rub} alt="r" />
+            </p>
         </div>
     );
 };
