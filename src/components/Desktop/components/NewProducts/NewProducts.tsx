419ad92a56c3f1751ffd6572d8a2051cf0ff5a99
Faviun <lightbringerlal@gmail.com>
2024-05-28 20:37:39 +0300

    newProduct

--- src/components/Desktop/components/NewProducts/NewProducts.tsx
+++ src/components/Desktop/components/NewProducts/NewProducts.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../../../config/firebase";
 import { load } from "../../../../features/products/productsSlice";
+import ProductCard from "../../../ProductCard/ProductCard";
 
 const NewProducts = () => {
     const shopName =
@@ -13,6 +14,7 @@ const NewProducts = () => {
     const dispatch = useDispatch();
     const products = useSelector((state: any) => state.products.filtered);
     const [loading, setLoading] = useState(false);
+    const i = 0;
 
     // получить данные для массива продуктов
     const itemStyle =
@@ -37,25 +39,26 @@ const NewProducts = () => {
             <h3 className="font-bold text-2xl">Новые продукты</h3>
             <ul className="flex justify-between items-stretch">
                 {!loading ? (
-                    products.slice(0, 4).map((product: any) => (
-                        <li key={Math.random()} className={itemStyle}>
-                            <img
-                                className="self-center h-[120px] max-w-[50%]"
-                                src={product.image}
-                                alt={product.id}
-                            />
-                            <div className="flex flex-col w-full">
-                                <p className="font-semibold text-[16px]">
-                                    {product.name}
-                                </p>
-                                <p className="font-bold text-[24px] text-amber-300">
-                                    {product.price}
-                                </p>
-                                <button className=" mt-auto bg-amber-300 rounded-lg px-2 py-1">
-                                    ДОБАВИТЬ
-                                </button>
-                            </div>
-                        </li>
+                    products.slice(i, i + 4).map((product: any) => (
+                        // <li key={Math.random()} className={itemStyle}>
+                        //     <img
+                        //         className="self-center h-[120px] max-w-[50%]"
+                        //         src={product.image}
+                        //         alt={product.id}
+                        //     />
+                        //     <div className="flex flex-col w-full">
+                        //         <p className="font-semibold text-[16px]">
+                        //             {product.name}
+                        //         </p>
+                        //         <p className="font-bold text-[24px] text-amber-300">
+                        //             {product.price}
+                        //         </p>
+                        //         <button className=" mt-auto bg-amber-300 rounded-lg px-2 py-1">
+                        //             ДОБАВИТЬ
+                        //         </button>
+                        //     </div>
+                        // </li>
+                        <ProductCard key={product.id} product={product} />
                     ))
                 ) : (
                     <p>Загрузка...</p>
---
+++ src/config/shops.ts
@@ -0,0 +1,7 @@
+export const shops = [
+    {id: 0, shopName: 'products', name: 'Шериф'},
+    {id: 1, shopName: 'drive', name: 'Кафе Drive'},
+    {id: 2, shopName: 'blagoda', name: 'Благода'},
+    {id: 3, shopName: 'miasko', name: 'Мяско'},
+    {id: 4, shopName: 'labagete', name: 'LaBagete'},
+    ]
