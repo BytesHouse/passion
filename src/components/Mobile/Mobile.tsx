d9b51a637db5f68780bd8ba61d922bb59b12f47c
a.lihtgolt <a.lihtgolt@gmail.com>
2024-06-05 11:07:21 +0300

    desktop update

--- src/components/Mobile/Mobile.tsx
+++ src/components/Mobile/Mobile.tsx
@@ -1,6 +1,7 @@
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
+import AddressComponent from "../AddressComponent/AddressComponent";
 
 interface MobileProps {
     handleShowModal: () => void;
