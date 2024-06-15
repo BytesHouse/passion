ac56c5f4e8a2c52ff506cddb79341c1187798029
favian <lightbringerlal@gmail.com>
2024-06-15 10:48:17 +0300

    bugFixOrder

--- src/components/DeliveryOrder/DeliveryOrder.tsx
+++ src/components/DeliveryOrder/DeliveryOrder.tsx
@@ -3,11 +3,16 @@ import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import OrderForm from "./OrderForm/OrderForm";
 import OrderList from "./OrderList/OrderList";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { load } from "../../features/cart/cartSlice";
+import { Link } from "react-router-dom";
+import ArrowLeft from "../../assets/icons/ArrowLeft/ArrowLeft";
 
 const DeliveryOrder = () => {
+    const { cart } = useSelector((state: any) => state.cart);
+    const cartLocal = JSON.parse(localStorage.getItem("cart") || cart);
     const dispatch = useDispatch();
+
     useEffect(() => {
         if (localStorage.getItem("cart") === null) {
             localStorage.setItem("cart", JSON.stringify([]));
@@ -20,10 +25,23 @@ const DeliveryOrder = () => {
     return (
         <>
             <Header />
-            <div className="lg:container mx-auto px-[20px] mt-[1.2em] w-full lg:w-[80%] flex flex-col-reverse lg:flex-row gap-[30px] font-[600] text-[2.5em] lg:text-[17px] lg:leading-[28px]">
+            {cart.length || cartLocal.length ? (
+                <div className="lg:container mx-auto px-[20px] mt-[1.2em] w-full lg:w-[80%] flex flex-col-reverse lg:flex-row gap-[30px] font-[600] text-[2.5em] lg:text-[17px] lg:leading-[28px]">
                     <OrderForm />
                     <OrderList />
-            </div>
+                </div>
+            ) : (
+                <div className="flex flex-col justify-center items-center mx-auto my-[100px] gap-[20px] ">
+                    <p className="text-[24px]">Корзина пуста</p>
+                    <Link
+                        to={"/"}
+                        className="border px-[150px] py-[30px] flex gap-[15px] items-center"
+                    >
+                        <ArrowLeft />
+                        <span>Назад</span>
+                    </Link>
+                </div>
+            )}
             <Footer />
         </>
     );
